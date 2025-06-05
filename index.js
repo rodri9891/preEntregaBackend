c36a700551eba760841ef839752bb7e249ef013f
rodri9891 <rodri.9891@hotmail.com>
2025-06-05 03:45:40 -0300

    busqueda con id de requerimiento 2 completa

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-async function obtenerDatos() {
+async function obtenerDatos(url) {
     try {
-        const response = await fetch("https://fakestoreapi.com/products"); 
+        const response = await fetch(`https://fakestoreapi.com/${url}`); 
         const datos = await response.json(); 
         return datos; 
     } catch (error) {
@@ -8,28 +8,39 @@ async function obtenerDatos() {
         return []; 
     }
 }
-/*
-(async function (){
-    const arrayDeDatos = await obtenerDatos();
-    console.log(arrayDeDatos.slice(0,2));
-})();
-*/
+
 
 const argumentos = process.argv.slice(2);
 const [comando1,...comandos] = argumentos;
+//const [comando1, parametro] = argumentos;
 
 (async function() {
     switch(comando1){
-    case "GET":
-        const productos = await obtenerDatos();
-        console.log("Se muestran todos los productos", productos);
-        break
-    case "POST":
-        console.log("producto creado:", comandos[0]);
-        break
-    case "PUT":
-        console.log("producto con ID modificado es:", comandos[0]);
-        break
-    case "DELETE":
-        console.log("producto eliminado:", comandos[0])
-}})();
+        case "GET":
+            if(comandos[1]){
+                const id =  Number(comandos[1]);
+                const productos = await obtenerDatos(comandos[0]);
+                const productoid = await productos.find(prod => prod.id === id);
+            
+            if (productoid){
+                console.log("el producto solicitado es: ", productoid);
+            } else{
+                console.log("no se encontro el producto");
+            }
+            
+            }else{
+                const productos = await obtenerDatos(comandos[0]);
+                console.log("Se muestran todos los productos", productos);
+            }
+            
+            break
+        case "POST":
+            console.log("producto creado:", comandos[0]);
+            //id con todos los datos
+            break
+        case "PUT":
+            console.log("producto con ID modificado es:", comandos[0]);
+            break
+        case "DELETE":
+            console.log("producto eliminado:", comandos[0])
+    }})();
