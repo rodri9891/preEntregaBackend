8584da980b01cddd44b7113a39a7afe869120a0f
rodri9891 <rodri.9891@hotmail.com>
2025-06-04 22:54:48 -0300

    requerimiento 1 y 2 a completar

--- index.js
+++ index.js
@@ -1,33 +1,35 @@
-const URL = `https://fakestoreapi.com/products`;
-const fetchData = async (url) =>{
-    try{
-        const response = await fetch(url);
-        const data = await response.json();
-        console.log(data);
-    } catch(err){
-        console.log(err);
+async function obtenerDatos() {
+    try {
+        const response = await fetch("https://fakestoreapi.com/products"); 
+        const datos = await response.json(); 
+        return datos; 
+    } catch (error) {
+        console.error("Error", error);
+        return []; 
     }
-};
+}
+/*
+(async function (){
+    const arrayDeDatos = await obtenerDatos();
+    console.log(arrayDeDatos.slice(0,2));
+})();
+*/
 
-//fetchData(URL)
-const Productos = fetchData(URL);
-
-
-
-const argumentos = Productos.process.argv.slice(2);
-console.log(argumentos);
+const argumentos = process.argv.slice(2);
 const [comando1,...comandos] = argumentos;
 
-switch(comando1){
+(async function() {
+    switch(comando1){
     case "GET":
-        console.log("Se muestran odos los productos");
+        const productos = await obtenerDatos();
+        console.log("Se muestran todos los productos", productos);
         break
     case "POST":
-        console.log("producto creado:" , comandos[0]);
+        console.log("producto creado:", comandos[0]);
         break
     case "PUT":
-        console.log("producto con ID modificado es:". comandos[0]);
+        console.log("producto con ID modificado es:", comandos[0]);
         break
     case "DELETE":
         console.log("producto eliminado:", comandos[0])
-}
+}})();
